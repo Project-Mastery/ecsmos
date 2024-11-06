e1c7c711b13e6ca457ba67292644dcd35f33b862
RafaelCotrim <rafael.souza.cotrim@gmail.com>
2024-11-06 17:48:40 +0000

    chore: Fix rust analizer path errors

--- flake.nix
+++ flake.nix
@@ -20,6 +20,8 @@
         xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
         # libxkbcommon wayland # To use the wayland feature
       ];
+
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
   };
 }
